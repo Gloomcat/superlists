ca4dbc7362bb76ce50cabb76cb12cf82f08ce887
gloomcat <reneguarde@gmail.com>
2015-12-06 22:29:42 +0200

    Home page now saves POST request

--- functional_tests.py
+++ functional_tests.py
@@ -26,10 +26,15 @@ class NewVisitorTest(unittest.TestCase):
 		input_box.send_keys('Buy peacock feathers')
 		input_box.send_keys(Keys.ENTER)
 
-		table = self.browser.get_element_by_id('id_list_table')
-		rows = self.browser.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+		input_box = self.browser.find_element_by_id('id_new_item')
+		input_box.send_keys('Use peacock feathers to make a fly')
+		input_box.send_keys(Keys.ENTER)
 		
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = self.browser.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
 		self.fail('Finish the test!')
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
+from django.template.context_processors import csrf
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
@@ -13,7 +14,20 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		expected_html = render_to_string('home.html')
+		
+		context = csrf(request)
+
+		expected_html = render_to_string('home.html', context)
 		self.assertEqual(response.content.decode(), expected_html)
 
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		response = home_page(request)
 
+		context = csrf(request)
+		context.update({'new_item_text': 'A new list item'})
+		
+		expected_html = render_to_string('home.html', context)
+		self.assertEqual(response.content.decode(), expected_html)
